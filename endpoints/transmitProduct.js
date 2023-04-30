a043569ab7a37b9758062d12437c29724517a0a0
sudokoko <joebiden@kitgaming67.com>
2023-04-30 10:02:57 -0400

    Attempt to use destructured declaration for req body

--- endpoints/transmitProduct.js
+++ endpoints/transmitProduct.js
@@ -1,26 +1,38 @@
 module.exports = function (databaseConnection, http, loggingHandler) {
   http.post("/services/textdb/post", async (req, res) => {
-      let product = req.body;
-      await databaseConnection.query(
-          `INSERT INTO "products" ("issueAt", "expireAt", "vtecString", "locationData", "raw")  
-             VALUES ($1, $2, $3, $4, $5)`, [product.issueAt, product.expireAt, product.vtecString,
-                                            product.locationData, product.raw],
-          (queryError, queryResponse) => {
-              if (queryError) {
-                  loggingHandler.write("error", queryError.stack);
-                  return res.json({
-                      code: 500,
-                      message:
-                          "An error occurred whilst attempting to transmit the product.",
-                  });
-              } else {
-                  loggingHandler.write(
-                      "info",
-                      `Storing text product with VTEC String (${product.vtecString ?? "NO VTEC"})`
-                  );
-                  return res.json({code:200, message:"Product stored in TextDB successfully.", product: queryResponse.rows[0]});
-              }
-          }
-      );
+    let {expireAt, issueAt, locationData, raw, vtecString} = req.body;
+    await databaseConnection.query(
+      `INSERT INTO "products" ("issueAt", "expireAt", "vtecString", "locationData", "raw")  
+             VALUES ($1, $2, $3, $4, $5)`,
+      [
+        issueAt,
+        expireAt,
+        vtecString,
+        locationData,
+        raw,
+      ],
+      (queryError, queryResponse) => {
+        if (queryError) {
+          loggingHandler.write("error", queryError.stack);
+          return res.json({
+            code: 500,
+            message:
+              "An error occurred whilst attempting to transmit the product.",
+          });
+        } else {
+          loggingHandler.write(
+            "info",
+            `Storing text product with VTEC String (${
+              vtecString ?? "NO VTEC"
+            })`
+          );
+          return res.json({
+            code: 200,
+            message: "Product stored in TextDB successfully.",
+            product: queryResponse.rows[0],
+          });
+        }
+      }
+    );
   });
 };
